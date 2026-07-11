7b1bc98581144e95ba80a58f8e106dbc0f32eb2a
AgnanKen <agnan.jaisil@gmail.com>
2026-07-11 09:47:59 +0530

    Student UI

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddStudent = () => {
+  return (
+    <div>AddStudent
+
+        <h1>Welcome to Add Student Page</h1>
+    </div>
+  )
+}
+
+export default AddStudent
