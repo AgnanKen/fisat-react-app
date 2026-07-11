fd53a5976f93ea3bf9a855c1f3d8a8aec8de9d05
AgnanKen <agnan.jaisil@gmail.com>
2026-07-11 10:29:17 +0530

    Student Search UI

---
+++ src/components/SearchStudent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchStudent = () => {
+  return (
+    <div>
+        <h1>Welcome to Search Student Page</h1>
+    </div>
+  )
+}
+
+export default SearchStudent
